e58222e11f434de2c12acbce0402a96e64ac7cb1
Riccardo Cafagna <riccafagna@gmail.com>
2019-02-19 17:09:24 -0300

    Gravidade & Collision (Up Only)

--- RPG Project/RPG Project/MainPage.xaml.cs
+++ RPG Project/RPG Project/MainPage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using RPG_Noelf.Assets;
-using RPG_Noelf.Assets.Scripts.Player;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -7,7 +6,6 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -15,46 +13,29 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Drawing;
+using Windows.UI.Xaml.Media.Imaging;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace RPG_Noelf
+namespace RPG_Project
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        Character player;
-
         public MainPage()
         {
             this.InitializeComponent();
-            
-        }
-        
-        private void MakeHappen_Click(object sender, RoutedEventArgs e)
-        {
-            GeradorFoto.MergeImages(Personagem, 197, 202, RenderedImage);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            player.Jump();
+            Windows.UI.Xaml.Controls.Image _head;
+            BitmapSource bit = (BitmapSource) head.Source;
+            bit.
+            Bitmap newFoto = GeradorFoto.MergeImages(head, eyes);   
         }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            if(player == null)
-            {
-                player = new Character(Player, PlayerCanvas);
-                player.setFloorPos(BlockPos);
-                player.setPlayerPosText(PlayerPos);
-            }
-            player.UpdateBlocks(Chunck01);
-            player.constant = double.Parse(Constant?.Text);
-            player.ResetPosition(320, 40);
-        }
-        
     }
 }
