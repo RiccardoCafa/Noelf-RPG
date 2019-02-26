f7cd0c244a1b3df8d68861e775bec624120ceb2a
Lucas Souza <42611011+LucasSNSantos@users.noreply.github.com>
2019-02-26 10:56:26 -0300

    added RunaArv
    Added RunaArv
    
    Co-Authored-By: Miguel Falcão  <mvfalcao@users.noreply.github.com>

--- RPG Noelf/RPG Noelf/Assets/Scripts/InventoryScripts/Item.cs
+++ RPG Noelf/RPG Noelf/Assets/Scripts/InventoryScripts/Item.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace RPG_Noelf.Assets.Images.Inventory_Scripts
 {
-    public enum Category
-    {
-        Normal,Magical,Epic, Legendary
-    }
+
+        public enum Category//definição de raridade do item
+        {
+            Normal,Magical,Epic, Legendary
+        }
 
   
       class Item
@@ -19,7 +20,7 @@ namespace RPG_Noelf.Assets.Images.Inventory_Scripts
         public int amount { get; set; } // quantidade de um mesmo item
         public string name { get; set; } // nome do item;
         public bool isStackable;//se é possivel acumular ou não
-        public Category itemCategory { get; set; }
+        public Category itemCategory { get; set; }//raridade do item
         public string itemID { get; set; }// ID de indentificação dos itens
         
     }
---
+++ RPG Noelf/RPG Noelf/Assets/Scripts/Runas/RunaArv.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Noelf.Assets.Scripts.Runas
+{
+    class RunaArv
+    {
+        public RunaArv atq;
+        public RunaArv defense;
+        public RunaArv skill;
+        public RunaArv LifeRegen;
+        public RunaArv ManaRegen;
+
+        public RunaArv()
+        {
+            atq = new RunaArv();
+            defense = new RunaArv();
+            skill = new RunaArv();
+            LifeRegen = new RunaArv();
+            ManaRegen = new RunaArv();
+        }
+
+       
+
+
+    }
+}
