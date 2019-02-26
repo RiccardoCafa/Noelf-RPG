fc34ef510cafde4a7dcc0a07b062f3e216b3a5a8
oLucasRez <Lucas_rez@outlook.com>
2019-02-26 15:38:33 -0300

    class, race, atribute, player

---
+++ RPG Noelf/RPG Noelf/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Noelf.Assets.Scripts.Player
+{
+    class Player
+    {
+        public Race Race { get; set; }
+        public int Hp { get; set; }
+        public int HpMax { get; set; }
+        public int Xp { get; private set; }
+        public int XpLim { get; private set; }
+        public int Mp { get; set; }
+        public int MpMax { get; set; }
+        public int Armor { get; set; }
+        public int Damage { get; set; }
+        public int Level { get; private set; } = 1;
+        public double AtkSpd { get; set; }
+
+        public bool XpLevel(int xp)//responde se passou de nivel ou nao, alem de upar
+        {
+            Xp += xp;
+            if(Xp >= XpLim)
+            {
+                Xp -= XpLim;
+                Level++;
+                return true;
+            }
+            return false;
+        }
+
+        public void LevelUpdate(int str, int spd, int dex, int con, int mnd)//atualiza os atributos ao upar
+        {
+            HpMax = Race.Con * 6 + Level * 2;
+            Hp = HpMax;
+            XpLim = Level * 100 + (int) Math.Pow(1.5, Level / 4);
+            MpMax = Race.Mnd * 5 + Level;
+            Mp = MpMax;
+            AtkSpd = 2 - 1.75 * Race.Spd / 100;
+        }
+    }
+}
---
+++ RPG Noelf/RPG Noelf/Assets/Scripts/Player/Race.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Noelf.Assets.Scripts.Player
+{
+    abstract class Race : IAtributes
+    {
+        public Class Class { get; set; }
+        public int Str { get; set; }
+        public int Spd { get; set; }
+        public int Dex { get; set; }
+        public int Con { get; set; }
+        public int Mnd { get; set; }
+    }
+
+    class Human : Race
+    {
+        public Human(Class @class)
+        {
+            Class = @class;
+            Str = 6 + @class.Str;
+            Spd = 4 + @class.Spd;
+            Dex = 5 + @class.Dex;
+            Con = 4 + @class.Con;
+            Mnd = 6 + @class.Mnd;
+        }
+    }
+
+    class Orc : Race
+    {
+        public Orc(Class @class)
+        {
+            Class = @class;
+            Str = 9 + @class.Str;
+            Spd = 2 + @class.Spd;
+            Dex = 3 + @class.Dex;
+            Con = 9 + @class.Con;
+            Mnd = 2 + @class.Mnd;
+        }
+    }
+
+    class Elf : Race
+    {
+        public Elf(Class @class)
+        {
+            Class = @class;
+            Str = 2 + @class.Str;
+            Spd = 7 + @class.Spd;
+            Dex = 7 + @class.Dex;
+            Con = 2 + @class.Con;
+            Mnd = 7 + @class.Mnd;
+        }
+    }
+}
