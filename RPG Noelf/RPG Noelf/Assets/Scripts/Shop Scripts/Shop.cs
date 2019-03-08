9ac22200d1ff564955cf2381f660ba6201a28555
Lucas Souza <42611011+LucasSNSantos@users.noreply.github.com>
2019-03-08 17:55:28 -0300

    added Shop

---
+++ RPG Noelf/RPG Noelf/Assets/Scripts/Shop Scripts/Shop.cs
@@ -0,0 +1,27 @@
+﻿using RPG_Noelf.Assets.Scripts.Inventory_Scripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Noelf.Assets.Scripts.Shop_Scripts
+{
+    class Shop
+    {
+        public List<Item> itens;
+
+        public Item sellItem(Item selled, Bag playerbag)
+        {
+
+            if(playerbag.nGold > selled.goldValue && playerbag.canAddMore() == true)
+            {
+
+                playerbag.addToBag
+
+            }
+
+
+        }
+    }
+}
