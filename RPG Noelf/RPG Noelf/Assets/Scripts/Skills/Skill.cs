a4b15e1c509c48d5a29ba7f938cf85b7e59ef4f0
Miguel Falcão <MVFalcao@users.noreply.github.com>
2019-03-08 22:12:35 -0300

    Skills

---
+++ RPG Noelf/RPG Noelf/Assets/Scripts/Skills/Skill.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Noelf.Assets.Scripts.Skills
+{
+    class Skill
+    {
+        public float damage { get; }
+        private float manac;
+        public int Lvl { get; set; } = 1;
+        public int block { get; }
+        private float bonusD;
+        private float DamageBonus;
+        public char type { get; }
+        private Boolean area;
+        private char atrib;
+
+        public void UseSkill(Player p, Skill a,Player Enemy)
+        {
+            if (a.manac <= p.mana)
+            {
+                p.mana -= a.manac;
+                Enemy.Hp -= damage + bonusD;
+            }
+            
+
+
+        }
+        public Skill(float d, float m, int b, float bd, char t,char a)
+        {
+            this.damage = d;
+            this.manac = m;
+            this.block = b;
+            this.bonusD = bd;
+            this.type = t;
+            this.atrib = a;
+        }
+        public void CalcBonus(Player calcP)
+        {
+            if(atrib == 'F')
+            {
+                DamageBonus = calcP.force * bonusD; 
+            }
+        }
+    }
+}
---
+++ RPG Noelf/RPG Noelf/Assets/Scripts/Skills/SkillManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Noelf.Assets.Scripts.Skills
+{
+    class SkillManager
+    {
+        List<Skill> SkillList;
+        /*public void TestLvl(player a, Skill b)
+        {
+            if (a.lvl > b.block)
+            {
+                UpSkill(b);
+            }
+            else
+            {
+                //mensagem
+            }
+        }*/
+        public void UpSkill(Skill b)
+        {
+            if (b.type == 'P')
+            {
+                if (b.Lvl <= 15)
+                {
+                    b.Lvl++;
+                }
+            }
+            else if (b.type == 'R')
+            {
+                if (b.Lvl < 10)
+                {
+                    b.Lvl++;
+                }
+            }
+            else
+            {
+                if (b.Lvl < 25)
+                {
+                    b.Lvl++;
+                }
+            }
+
+        }
+        /*public void TrocaSkill(Player perso, Skill velha, Skill nova){
+	        if(velha.type == 'P'){
+	            //n pode
+	            }else{
+	                int indexnew, indexold;
+                    Skill olds, news;
+                    indexold = velha.FindIndex(a=>a.Prop == aProp);
+	                indexnew = nova.FindIndex(a=>a.Prop == aProp);
+                    olds = velha;
+	                news = nova;
+	                perso.SkillList.insert(news, indexold);
+	                perso.SkillList.insert(olds, indexnew);
+	            }
+
+        }*/
+	}
+}
+    
+
