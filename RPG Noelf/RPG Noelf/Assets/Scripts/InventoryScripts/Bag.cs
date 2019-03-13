9c33f702552d91142943a7ec4c300cd6baa36a8a
Lucas Souza <42611011+LucasSNSantos@users.noreply.github.com>
2019-03-13 10:44:20 -0300

    Startd Drop System
    started MobSlot
    Started PlayerSlot
    Fixed Encyclopedia
    Changed Bag

--- RPG Noelf/RPG Noelf/Assets/Scripts/InventoryScripts/Bag.cs
+++ RPG Noelf/RPG Noelf/Assets/Scripts/InventoryScripts/Bag.cs
@@ -10,58 +10,18 @@ using System.Threading.Tasks;
 
 namespace RPG_Noelf.Assets.Scripts.Inventory_Scripts
 {
-    class Bag
+    abstract class Bag
     {
-        public const uint maxStack = 99;
-        public int freeSlots { get; set; } //numero de  espaços livres no inventário do player
-        public int nGold { get; set; } // dinheiro do player;
-        public List<SlotInventory> slots { get; set; } //espaços para guardar os itens
-        public Bag() //precisa de construtor?
-        {
-            slots = new List<SlotInventory>();
-            this.freeSlots = 30;
-            
-        }
-        //resolvido o problema de adição na mochila com a criação de 3 funções extras
-        public bool AddToBag(uint itemID, uint amount)
-        {
-            if(freeSlots>0 && Encyclopedia.SearchStackID(itemID) == false)
-            {
-                SlotInventory ad = new SlotInventory(itemID, amount);
-                slots.Add(ad);
-                freeSlots--;
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+       
+        public int freeSlots { get; set; } 
+        public int Gold { get; set; } 
+       
+ 
+        public abstract bool AddToBag(uint itemID, uint amount);
+        public abstract SlotInventory DropFromBag(uint itemID, uint amount);//remove o item da bag
 
-
-        }
-        // atribuir o item para null é o suficiente para remove-lo, pois o GB ira eventualmente limpar
-        public void RemoveFromBag(uint itemID, uint amount)//remove o item da bag
-        {
-
-        }
-        public void AddGold(int coins) // incrementa o gold na mochila
-        {
-            nGold = nGold + coins;
-        }
-        public bool CashOut(int coins)//retorna o dinheiro que sobrou
-        {
-            if (nGold < coins)
-            {
-                //não tenho dinheiro suficiente
-                return false;
-            }
-            else
-            {
-                nGold = nGold - coins;
-                return true;
-            }
-
-        }
+        public abstract bool AddGold(int coins); //adicionar gold na mochila
+        
         public uint AmountInBag(uint itemID) //retorna a quantidade de um item especifico na mochila
         {
            
@@ -73,18 +33,7 @@ namespace RPG_Noelf.Assets.Scripts.Inventory_Scripts
 
             return 0;
         }
-        public bool SearchID(int itemID)//verifica se o ID esta na mochila
-        {
-            foreach(SlotInventory sl in slots)
-            {
-                if(sl.itemID == itemID)
-                {
-                    return true;
-                }
-            }
-            return false;            
-        }
-       
+
         public bool CanAddMore()
         {
             if (this.freeSlots > 0) return true;
