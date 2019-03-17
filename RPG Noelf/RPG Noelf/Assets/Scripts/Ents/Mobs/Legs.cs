d9547fed85a15c46362eb8d9c26fc5206578d5cd
Miguel Falcão <MVFalcao@users.noreply.github.com>
2019-03-17 19:37:18 -0300

    skills/shop

--- RPG Noelf/RPG Noelf/Assets/Scripts/Ents/Mobs/Legs.cs
+++ RPG Noelf/RPG Noelf/Assets/Scripts/Ents/Mobs/Legs.cs
@@ -54,7 +54,7 @@ namespace RPG_Noelf.Assets.Scripts.Ents.Mobs
         public CatLegs(Mob mob) : base(mob)
         {
             mob.Dex += 1;
-            mob.vulnerable.Add(Element.common);
+            mob.vulnerable.Add(Element.none);
         }
     }
 }
