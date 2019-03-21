f0eeab5e665081bce9ca766ed822154b0ee5da8f
oLucasRez <Lucas_rez@outlook.com>
2019-03-21 15:18:30 -0300

    mob

---
+++ RPG Noelf/RPG Noelf/Assets/Scripts/Scenes/Scene.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace RPG_Noelf.Assets.Scripts.Scenes
+{
+    class Scene
+    {
+        public string[] Blueprint { get; private set; } = new string[15];
+
+        public Scene(Canvas canvas)
+        {
+            Blueprint[00] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            Blueprint[01] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            Blueprint[02] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            Blueprint[03] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            Blueprint[04] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            Blueprint[05] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaGGGGGGGGGGGGGGGGGGaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            Blueprint[06] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaGaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            Blueprint[07] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaGaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            Blueprint[08] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaGGGGGaaaaaaaaaaaaaaaaaGGGGG";
+            Blueprint[09] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaGGGGGaaaaaaaGGGggggg";
+            Blueprint[10] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaGGgggggggg";
+            Blueprint[11] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaGgggggggggg";
+            Blueprint[12] = "aaaaaaaaaaaaaaaGGGGGGGGGGGGGGGGGGGGaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaGGggggggggggg";
+            Blueprint[13] = "GGGGGGGGGGGGGGGggggggggggggggggggggaaaaaaaaaaaaaaaaaaaaaaaaaaaaGGGGGGGGGGGGGGGGGGGGGGGGggggggggggggg";
+            Blueprint[14] = "gggggggggggggggggggggggggggggggggggGGGGGGGGGGGGGGGGGGGGGGGGGGGGggggggggggggggggggggggggggggggggggggg";
+            //Blueprint = Blueprint.Reverse().ToString();
+            for(int i = 0; i < 15; i++)
+            {
+                int j = 0;
+                foreach (char block in Blueprint[i])
+                {
+                    if(block != 'a')
+                    {
+                        Tile tile = new Tile(Tile.TileCode[block], i, j);
+                        Image image = new Image();
+                        image.Source = new BitmapImage(new Uri(MainPage.instance.BaseUri, tile.Path));
+                        image.Width = tile.VirtualSize[0];
+                        image.Height = tile.VirtualSize[1];
+                        //image.SetValue.
+                        canvas.Children.Add(image);
+                    }
+                    j++;
+                }
+            }
+        }
+    }
+}
---
+++ RPG Noelf/RPG Noelf/Assets/Scripts/Scenes/Tile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Noelf.Assets.Scripts.Scenes
+{
+    enum TypeTile
+    {
+        grass, ground
+    }
+
+    class Tile
+    {
+        public static Dictionary<char, TypeTile> TileCode = new Dictionary<char, TypeTile>()
+        {
+            {'G', TypeTile.grass },
+            {'g', TypeTile.ground }
+        };
+
+        public TypeTile tpTile;
+        public double[] vetor = new double[2];
+        public string Path { get; private set; }
+        public double[] Size { get; } = { 60, 60 };
+        public double[] VirtualSize { get; } = { 60, 115 };
+        public double[] VirtualPosition { get; private set; }
+
+        public Tile(TypeTile tpTile, double x, double y)
+        {
+            this.tpTile = tpTile;
+            vetor[0] = x * 60;
+            vetor[1] = y * 60;
+            Path = "/Assets/Images/" + tpTile + ".png";
+            VirtualPosition[0] = vetor[0];
+            VirtualPosition[1] = vetor[1] - 40;
+        }
+    }
+}
