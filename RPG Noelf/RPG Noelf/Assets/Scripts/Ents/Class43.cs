e87d8d95f7bc83160fc2603d8f40b9d6841c4ecc
RiccardoCafa <riccafagna@gmail.com>
2019-03-23 01:21:12 -0300

    Create Class43.cs

---
+++ RPG Noelf/RPG Noelf/Assets/Scripts/Ents/Class43.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Noelf.Assets.Scripts.Ents
+{
+    class Class43
+    {
+    }
+}
