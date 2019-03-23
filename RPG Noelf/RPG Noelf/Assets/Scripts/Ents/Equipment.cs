24e89dcd43acd27822ce8ca042f789ded5c8b0f5
zikawhite <felipestamoglou@gmail.com>
2019-03-23 01:02:39 -0300

    deu ruim, mas deu bom

--- RPG Noelf/RPG Noelf/Assets/Scripts/Ents/Equipment.cs
+++ RPG Noelf/RPG Noelf/Assets/Scripts/Ents/Equipment.cs
@@ -8,8 +8,8 @@ using RPG_Noelf.Assets.Scripts.PlayerFolder;
 
 
 namespace RPG_Noelf.Assets.Scripts.Equipment
-   
-    
+
+
 {
     class Equipment
     {
@@ -18,11 +18,11 @@ namespace RPG_Noelf.Assets.Scripts.Equipment
             this.player = player;
         }
 
-        public uint[] armor = new uint[3];//elmo 0, colete 1, pernas 2
+        public uint[] armor = new uint[3];//elm 0, armor 1, Legs 2.
         public uint weapon;
         private Player player;
 
-        public void UseEquip(uint ID)
+        public void UseEquip(uint ID)// function that makes the equipment be equipped by the player.
         {
             Item item = Encyclopedia.encyclopedia[ID];
             if (item is Armor)
@@ -30,19 +30,19 @@ namespace RPG_Noelf.Assets.Scripts.Equipment
                 Slot arm = player._Inventory.GetSlot(ID);
                 if (arm != null)
                 {
-                    switch((item as Armor).PositArmor)
+                    switch ((item as Armor).PositArmor) //swtich to know where the armor is
                     {
                         case PositionArmor.Elm:
                             armor[0] = ID;
-                            player._Inventory.RemoveFromBag(ID, 1);
+                            player._Inventory.RemoveFromBag(ID, 1);//equip elm
                             break;
                         case PositionArmor.Armor:
                             armor[1] = ID;
-                            player._Inventory.RemoveFromBag(ID, 1);
+                            player._Inventory.RemoveFromBag(ID, 1);//equip armor
                             break;
                         case PositionArmor.Legs:
                             armor[2] = ID;
-                            player._Inventory.RemoveFromBag(ID, 1);
+                            player._Inventory.RemoveFromBag(ID, 1);//equip legs
                             break;
                     }
                 }
@@ -53,39 +53,40 @@ namespace RPG_Noelf.Assets.Scripts.Equipment
                 if (weap != null)
                 {
                     weapon = ID;
-                    player._Inventory.RemoveFromBag(ID, 1);
+                    player._Inventory.RemoveFromBag(ID, 1);// equip weapon
                 }
             }
-        
+
         }
-        public void DesEquip(uint ID)
+        public void DesEquip(uint ID)//function that causes the equipment to be unbalanced by the player.
         {
             Item item = Encyclopedia.encyclopedia[ID];
-            if (item is Armor) {
+            if (item is Armor)
+            {
 
-                switch ((item as Armor).PositArmor)
+                switch ((item as Armor).PositArmor)//swtich to know where the armor is
                 {
-                case PositionArmor.Elm:
-                        
-                    if (player._Inventory.AddToBag(ID, 1))
-                    {
-                        armor[0] = 0;
-                    }
-                    break;
-                case PositionArmor.Armor:
-                        
-                    if (player._Inventory.AddToBag(ID, 1))
-                    {
-                        armor[1] = 0;
-                    }
-                    break;
-                case PositionArmor.Legs:
-                        
-                    if (player._Inventory.AddToBag(ID, 1))
-                    {
-                        armor[2] = 0;
-                    }
-                    break;
+                    case PositionArmor.Elm:
+
+                        if (player._Inventory.AddToBag(ID, 1))//unequip elm
+                        {
+                            armor[0] = 0;
+                        }
+                        break;
+                    case PositionArmor.Armor:
+
+                        if (player._Inventory.AddToBag(ID, 1))//unequip armor
+                        {
+                            armor[1] = 0;
+                        }
+                        break;
+                    case PositionArmor.Legs:
+
+                        if (player._Inventory.AddToBag(ID, 1))//unequip legs
+                        {
+                            armor[2] = 0;
+                        }
+                        break;
                 }
             }
             else if (item is Weapon)
@@ -93,11 +94,10 @@ namespace RPG_Noelf.Assets.Scripts.Equipment
                 Slot weap = player._Inventory.GetSlot(ID);
                 if (weap != null)
                 {
-                    
-                    if(player._Inventory.AddToBag(ID, 1)) weapon = 0;
+
+                    if (player._Inventory.AddToBag(ID, 1)) weapon = 0;//unequip weapon
                 }
             }
         }
     }
 }
-
