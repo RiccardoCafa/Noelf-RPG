b00f3dcde06eb1747652295ac7a0ffc49085232c
RiccardoCafa <riccafagna@gmail.com>
2019-03-29 20:33:26 -0300

    NPC's classes

---
+++ RPG Noelf/RPG Noelf/Assets/Scripts/Ents/NPCs/CharacterNPC.cs
@@ -0,0 +1,43 @@
+﻿using RPG_Noelf.Assets.Scripts.PlayerFolder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+
+namespace RPG_Noelf.Assets.Scripts.Ents.NPCs
+{
+    public enum NPCStates {
+        Interacting,
+        Stop,
+        Attacking,
+        Following,
+        Trading
+    }
+
+    class CharacterNPC : Character
+    {
+        public NPC MyNPC;
+        private Thread Update;
+
+        public CharacterNPC(Canvas T, NPC _NPC) : base(T)
+        {
+            MyNPC = _NPC;
+            Update = new Thread(update);
+        }
+
+        private async void update()
+        {
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+
+            });
+
+
+        }
+
+    }
+}
