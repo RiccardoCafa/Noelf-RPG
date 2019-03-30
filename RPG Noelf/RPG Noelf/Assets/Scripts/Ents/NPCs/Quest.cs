ae547e200921a2b79fca386e451265e9112677ee
Lucas Souza <42611011+LucasSNSantos@users.noreply.github.com>
2019-03-29 21:06:06 -0300

    Quests

---
+++ RPG Noelf/RPG Noelf/Assets/Scripts/Ents/NPCs/Quest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Noelf.Assets.Scripts.Ents.NPCs
+{
+    public enum TypeQuest
+    {
+        Collecting,Hunting,Boss,Searching,Speaking
+
+    }
+
+
+
+    class Quest
+    {
+        public string Description { get; set; }
+
+
+    }
+}
---
+++ RPG Noelf/RPG Noelf/Assets/Scripts/Ents/NPCs/QuestManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Noelf.Assets.Scripts.Ents.NPCs
+{
+    class QuestManager
+    {
+
+        public QuestManager(TypeQuest tpQuest)
+        {
+            //gerencia do tipo
+        }
+
+    }
+}
