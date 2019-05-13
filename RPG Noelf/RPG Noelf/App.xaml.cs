46ec130a37835fc98c03fe1a09ecd20db642becf
RiccardoCafa <riccafagna@gmail.com>
2019-05-12 22:52:00 -0300

    Update App.xaml.cs

--- RPG Noelf/RPG Noelf/App.xaml.cs
+++ RPG Noelf/RPG Noelf/App.xaml.cs
@@ -66,7 +66,7 @@ namespace RPG_Noelf
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(Menu), e.Arguments);
+                    rootFrame.Navigate(typeof(Game), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
